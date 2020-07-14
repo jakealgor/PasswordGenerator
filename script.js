47fc743219dbea390a73b5e35ba136b9ec632504
Jake Algor <jake.algor@gmail.com>
2020-07-14 19:59:28 -0400

    first commit

---
+++ script.js
@@ -0,0 +1,42 @@
+
+function generate(){
+
+    let complexity = document.getElementById("slider").value;
+
+    let values = "ABCDEFGHIJKLMNOPQRSTUVWZYZabcdefghijklmnopqrstuvwxyz1234567890!@#$%^&*()_+";
+
+    let password = "";
+
+    for(var i = 0; i <= complexity; i++){
+        password = password + values.charAt(Math.floor(Math.random() * Math.floor(values.length - 1)));
+    }
+
+    document.getElementById("display").value = password;
+
+
+    document.getElementById("lastNums").innerHTML += password + "<br />";
+
+}
+
+document.getElementById("length").innerHTML = "Length: 29";
+
+document.getElementById("slider").oninput = function(){
+
+    if(document.getElementById("slider").value > 8){
+        document.getElementById("length").innerHTML = "Length: " + document.getElementById("slider").value;
+    }
+    else{
+        document.getElementById("length").innerHTML = "Length: 8";
+    }
+
+}
+
+function copyPassword(){
+
+    document.getElementById("display").select();
+
+    document.execCommand("Copy");
+
+    alert("Password copied to clipboard!");
+
+}
